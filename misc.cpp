e1f27ff9f97ab83b0607b35e57179338d4bff41c
Michael Taboada <ai5hf@hotmail.com>
2016-10-15 06:29:16 -0500

    Forgot a newline.

--- misc.cpp
+++ misc.cpp
@@ -39,7 +39,7 @@ return NULL;
 }
 ALLEGRO_DISPLAY* disp = al_create_display(640,480);
 if(!disp) {
-log("Could not create display.");
+log("Could not create display.\n");
 	al_uninstall_audio();
 al_uninstall_keyboard();
 al_uninstall_system();
